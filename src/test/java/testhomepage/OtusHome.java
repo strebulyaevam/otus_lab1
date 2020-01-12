6e29b14d5866516619b043ddd27a53bd5cb027f6
Maria <mstrebulyaeva@gmail.com>
2020-01-12 23:30:30 +0300

    Removed unused import

--- src/test/java/testhomepage/OtusHome.java
+++ src/test/java/testhomepage/OtusHome.java
@@ -5,7 +5,6 @@ import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
